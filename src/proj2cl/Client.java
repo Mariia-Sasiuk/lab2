86d9ccf1bc0c06307d39823b195086fd1a8d8625
Mariia Sasiuk <melody.mos1@gmail.com>
2015-09-01 11:53:54 +0300

    Model of Client

---
+++ src/proj2cl/Client.java
@@ -0,0 +1,19 @@
+package proj2cl;
+
+public class Client {
+	private String myname;
+	private  int id;
+	
+	public String getMyname() {
+		return myname;
+	}
+	public void setMyname(String myname) {
+		this.myname = myname;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
